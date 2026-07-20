80d21e94310b047bac6f1e4c561cc351afd34b6f
시락 <121562193+sirack3@users.noreply.github.com>
2026-07-20 11:05:15 +0700

    Delete test3.java

--- test3.java
+++
@@ -1,1 +0,0 @@
-import org.bukkit.entity.Player; import org.bukkit.profile.PlayerProfile; public class test3 { public void test(Player p) { PlayerProfile prof = p.getPlayerProfile(); prof.setName(" 시락\); p.setPlayerProfile(prof); } }
